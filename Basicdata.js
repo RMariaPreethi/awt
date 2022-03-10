63b4e3c6c2ac26898cc31ecbc5ce264468cd00fb
Maria Preethi <mariapreethi1212@gmail.com>
2022-03-10 11:35:52 +0530

    added a file

---
+++ Basicdata.js
@@ -0,0 +1,10 @@
+let empname = "name";
+let brand = "hyd";
+let getBranch = function()
+{
+    return Branch;
+}
+let setBranch=function(newBranch){
+    branch=newBranch;
+}
+export { getBranch, setBranch}
